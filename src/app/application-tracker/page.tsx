8cdc25286ee2db654b5662863d0d1412150ebae2
A Gadedjro <FGadedjro@gmail.com>
2025-11-02 01:38:54 +0000

    augment the opacity up to 90%

--- src/app/application-tracker/page.tsx
+++ src/app/application-tracker/page.tsx
@@ -92,7 +92,7 @@ function DocumentDisplayDialog({ title, content, onDownload }: { title: string; 
                     </Card>
                 </Button>
             </DialogTrigger>
-            <DialogContent className="max-w-4xl h-[90vh] flex flex-col bg-gradient-to-br from-accent/10 to-background">
+            <DialogContent className="max-w-4xl h-[90vh] flex flex-col bg-gradient-to-br from-accent/90 to-background">
                 <DialogHeader>
                     <DialogTitle>{title}</DialogTitle>
                 </DialogHeader>
